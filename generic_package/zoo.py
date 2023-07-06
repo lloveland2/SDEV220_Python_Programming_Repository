eb82e1d9dfbc51f8b9e668fbeb08a0169eef53da
lloveland2 <98292060+lloveland2@users.noreply.github.com>
2023-07-06 19:55:39 -0400

    Changes to be committed:  - modified:   Jupyter Notebook -  If...Else and While.ipynb  - new file:   Jupyter Notebook - Modules and Databases.ipynb  - new file:   demo_repo  - new file:   generic_package/__pycache__/zoo.cpython-310.pyc  - new file:   generic_package/zoo.py

---
+++ generic_package/zoo.py
@@ -0,0 +1,4 @@
+"""The zoo module exists solely to demonstrate how a module, and it's functions, can be imported."""
+def  hours():
+    """Prints the open hours of the zoo."""
+    print("Open 9-5 daily")
